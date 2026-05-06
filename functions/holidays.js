e9a18ab0806daac929e19ba44bb6f03331f28f5f
Brad Nabholz <bnabholz@gmail.com>
2026-05-06 14:58:25 -0400

    Set Content-Type to application/json

--- functions/holidays.js
+++ functions/holidays.js
@@ -1,9 +1,10 @@
 const data = require("../public/holiday-data.json")
 
 exports.handler = async (event, context) => {
+  const headers = { "Content-Type": "application/json" };
   if (process.env.API_KEY === event.queryStringParameters.api_key) {
-    return { statusCode: 200, body: JSON.stringify(data) };
+    return { statusCode: 200, headers, body: JSON.stringify(data) };
   } else {
-    return { statusCode: 429, body: JSON.stringify({message: "Please host your own version of this API https://github.com/blairanderson/holidays-api/"}) };
+    return { statusCode: 429, headers, body: JSON.stringify({message: "Please host your own version of this API https://github.com/blairanderson/holidays-api/"}) };
   }
 };
